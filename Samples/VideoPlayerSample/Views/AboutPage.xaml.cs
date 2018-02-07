26e2896e2aa63cc0d5836f4596c55a6d8f6d6161
Luis Alberto Peña Nuñez <luis.pena.nunez.developer@gmail.com>
2018-02-07 16:45:08 -0500

    Work In Progress

---
+++ Samples/VideoPlayerSample/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace VideoPlayerSample
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
